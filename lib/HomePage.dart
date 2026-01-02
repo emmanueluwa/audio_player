76982345d13d53bc42e35cef5139e24c2e63d694
emmanueluwa <oemmanuel.uwadiae@gmail.com>
2026-01-02 07:40:51 +0000

    adding banner to home page

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -17,22 +17,68 @@ class _HomePageState extends State<HomePage> {
         child: Scaffold(
           body: Column(
             children: [
+              Container(
+                margin: const EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ImageIcon(
+                      AssetImage("images/menu.png"),
+                      size: 28,
+                      color: Colors.black,
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.search),
+                        SizedBox(width: 10),
+                        Icon(Icons.notifications),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  ImageIcon(
-                    AssetImage("images/menu.png"),
-                    size: 28,
-                    color: Colors.black,
-                  ),
-                  Row(
-                    children: [
-                      Icon(Icons.search),
-                      SizedBox(width: 10),
-                      Icon(Icons.notifications),
-                    ],
+                  Container(
+                    margin: const EdgeInsets.only(left: 20),
+                    child: Text("Popular", style: TextStyle(fontSize: 30)),
                   ),
                 ],
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 180,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 0,
+                      left: -20,
+                      right: 0,
+                      child: Container(
+                        height: 180,
+                        child: PageView.builder(
+                          controller: PageController(viewportFraction: 0.8),
+                          itemCount: 5,
+                          itemBuilder: (_, i) {
+                            return Container(
+                              height: 180,
+                              width: MediaQuery.of(context).size.width,
+                              margin: const EdgeInsets.only(right: 10),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                image: DecorationImage(
+                                  image: AssetImage("images/home_img.jpg"),
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
