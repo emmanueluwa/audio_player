62af0fb7fd2bd3dac9ed61a2a490b12b2634c2c8
emmanueluwa <oemmanuel.uwadiae@gmail.com>
2026-01-06 06:44:19 +0000

    adding detail page route from homepage

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,6 +1,7 @@
 import 'dart:convert' show json;
 
 import 'package:audio_player/app_colours.dart' as AppColors;
+import 'package:audio_player/detail_audio_page.dart';
 import 'package:audio_player/my_tabs.dart';
 import 'package:flutter/material.dart';
 
@@ -172,105 +173,118 @@ class _HomePageState extends State<HomePage>
                       ListView.builder(
                         itemCount: audios.length,
                         itemBuilder: (_, i) {
-                          return Container(
-                            margin: const EdgeInsets.only(
-                              left: 20,
-                              right: 20,
-                              top: 18,
-                              bottom: 10,
-                            ),
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => DetailAudioPage(),
+                                ),
+                              );
+                            },
+
                             child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: AppColors.tabVarViewColor,
-                                boxShadow: [
-                                  BoxShadow(
-                                    blurRadius: 2,
-                                    offset: Offset(0, 0),
-                                    color: Colors.grey.withValues(alpha: 0.2),
-                                  ),
-                                ],
+                              margin: const EdgeInsets.only(
+                                left: 20,
+                                right: 20,
+                                top: 18,
+                                bottom: 10,
                               ),
                               child: Container(
-                                padding: const EdgeInsets.all(8),
-                                child: Row(
-                                  children: [
-                                    Container(
-                                      width: 90,
-                                      height: 120,
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(10),
-                                        image: DecorationImage(
-                                          image: AssetImage(audios[i]["img"]),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: AppColors.tabVarViewColor,
+                                  boxShadow: [
+                                    BoxShadow(
+                                      blurRadius: 2,
+                                      offset: Offset(0, 0),
+                                      color: Colors.grey.withValues(alpha: 0.2),
+                                    ),
+                                  ],
+                                ),
+                                child: Container(
+                                  padding: const EdgeInsets.all(8),
+                                  child: Row(
+                                    children: [
+                                      Container(
+                                        width: 90,
+                                        height: 120,
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(
+                                            10,
+                                          ),
+                                          image: DecorationImage(
+                                            image: AssetImage(audios[i]["img"]),
+                                          ),
                                         ),
                                       ),
-                                    ),
-                                    SizedBox(width: 10),
-                                    Expanded(
-                                      child: Column(
-                                        children: [
-                                          Row(
-                                            children: [
-                                              Icon(
-                                                Icons.star,
-                                                size: 24,
-                                                color: AppColors.starColor,
-                                              ),
-                                              SizedBox(width: 5),
-                                              Text(
-                                                audios[i]["rating"],
-                                                style: TextStyle(
+                                      SizedBox(width: 10),
+                                      Expanded(
+                                        child: Column(
+                                          children: [
+                                            Row(
+                                              children: [
+                                                Icon(
+                                                  Icons.star,
+                                                  size: 24,
                                                   color: AppColors.starColor,
                                                 ),
-                                              ),
-                                            ],
-                                          ),
+                                                SizedBox(width: 5),
+                                                Text(
+                                                  audios[i]["rating"],
+                                                  style: TextStyle(
+                                                    color: AppColors.starColor,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
 
-                                          Text(
-                                            audios[i]["title"],
-                                            style: TextStyle(
-                                              fontSize: 16,
-                                              fontFamily: "Avenir",
-                                              fontWeight: FontWeight.bold,
-                                              color: AppColors.starColor,
-                                            ),
-                                            overflow: TextOverflow.ellipsis,
-                                            maxLines: 1,
-                                          ),
-                                          Text(
-                                            audios[i]["text"],
-                                            style: TextStyle(
-                                              fontSize: 16,
-                                              fontFamily: "Avenir",
-                                              fontWeight: FontWeight.bold,
-                                              color: AppColors.subTitleText,
-                                            ),
-                                            overflow: TextOverflow.ellipsis,
-                                            maxLines: 1,
-                                          ),
-                                          Container(
-                                            width: 60,
-                                            height: 20,
-                                            decoration: BoxDecoration(
-                                              borderRadius:
-                                                  BorderRadius.circular(3),
-                                              color: AppColors.loveColor,
-                                            ),
-                                            child: Text(
-                                              "Love",
+                                            Text(
+                                              audios[i]["title"],
                                               style: TextStyle(
-                                                fontSize: 12,
+                                                fontSize: 16,
                                                 fontFamily: "Avenir",
                                                 fontWeight: FontWeight.bold,
                                                 color: AppColors.starColor,
                                               ),
+                                              overflow: TextOverflow.ellipsis,
+                                              maxLines: 1,
                                             ),
-                                            alignment: Alignment.center,
-                                          ),
-                                        ],
+                                            Text(
+                                              audios[i]["text"],
+                                              style: TextStyle(
+                                                fontSize: 16,
+                                                fontFamily: "Avenir",
+                                                fontWeight: FontWeight.bold,
+                                                color: AppColors.subTitleText,
+                                              ),
+                                              overflow: TextOverflow.ellipsis,
+                                              maxLines: 1,
+                                            ),
+                                            Container(
+                                              width: 60,
+                                              height: 20,
+                                              decoration: BoxDecoration(
+                                                borderRadius:
+                                                    BorderRadius.circular(3),
+                                                color: AppColors.loveColor,
+                                              ),
+                                              child: Text(
+                                                "Love",
+                                                style: TextStyle(
+                                                  fontSize: 12,
+                                                  fontFamily: "Avenir",
+                                                  fontWeight: FontWeight.bold,
+                                                  color: AppColors.starColor,
+                                                ),
+                                              ),
+                                              alignment: Alignment.center,
+                                            ),
+                                          ],
+                                        ),
                                       ),
-                                    ),
-                                  ],
+                                    ],
+                                  ),
                                 ),
                               ),
                             ),
